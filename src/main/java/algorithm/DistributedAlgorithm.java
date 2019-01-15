31b45c00af30f7a56266a483c8ea26ae3f7dfc9c
RockyRoll <luoqi4110217@hotmail.com>
2019-01-15 20:48:06 +0800

    Distributed algorithm interface

---
+++ src/main/java/algorithm/DistributedAlgorithm.java
@@ -0,0 +1,11 @@
+package algorithm;
+
+import base.ResultSet;
+
+import java.util.ArrayList;
+
+public interface DistributedAlgorithm {
+    public ArrayList<ResultSet> run(String datasetName);
+
+    public ArrayList<ResultSet> run(String datasetName, double eta);
+}
